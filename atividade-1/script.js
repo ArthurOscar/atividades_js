468ae3cd4fd8e3f744a5a141f282402ca531c699
Arthur <arthur_o_soares@estudante.sesisenai.org.br>
2025-03-19 09:43:59 -0300

    UPDATE - Atividade 2 feita

--- atividade-1/script.js
+++
@@ -1,9 +0,0 @@
-function verificarPar(numero){
-    if(numero % 2 === 0){
-        console.log("par")
-    }else{
-        console.log("ímpar")
-    }
-}
-
-verificarPar(3)
---
+++ script.js
@@ -0,0 +1,21 @@
+//atividade 1
+function verificarPar(numero){
+    if(numero % 2 === 0){
+        console.log("par")
+    }else{
+        console.log("ímpar")
+    }
+}
+
+verificarPar(3)
+
+
+//atividade 2
+function matematicaBasica(numero, numero2){
+    console.log(numero + numero2)
+    console.log(numero * numero2)
+    console.log(numero / numero2)
+    console.log(numero - numero2)
+}
+
+matematicaBasica(4, 2)
